8c6672ae44849b01fbc04305852ef66fb407630a
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-09-25 22:02:33 -0700

    fix: Replace usage of next/router with the proper slug handling according to next.js documentation

--- app/reserve/[id]/page.tsx
+++ app/reserve/[id]/page.tsx
@@ -1,10 +1,7 @@
-import { useRouter } from "next/router";
 import React from "react";
 
-const router = useRouter();
-
-const ReserveRoomPage = () => {
-  return <div>ReserveRoomPage {router.query.id}</div>;
+const ReserveRoomPage = ({ params }: { params: { slug: string } }) => {
+  return <div>ReserveRoomPage {params.slug}</div>;
 };
 
 export default ReserveRoomPage;
