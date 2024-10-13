67aeb4f2b352cf2d5b0ef3c14bb14aa08678a47b
Mau <79354255+Luceium@users.noreply.github.com>
2024-10-13 02:13:18 -0700

    add link to improvement

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function HomePage() {
       {hotels.map((hotel) => (
         <Suspense fallback={<h1>loading...</h1>}>
           {/* Allows hotels to be loaded individually and not impede the first contentful paint*/}
+          {/* This is a perfect oppportunity to use https://react.dev/reference/react/Suspense#showing-stale-content-while-fresh-content-is-loading */}
           <div key={hotel}>{hotel}</div>
         </Suspense>
       ))}
