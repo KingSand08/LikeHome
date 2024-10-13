6ecc8bc3a8b5f7fe4f5b78ee599091847332776f
Mau <79354255+Luceium@users.noreply.github.com>
2024-10-12 18:39:07 -0700

    Refactors page structure Adds new page for hotel room Reorganizes a few pages Adds new components that were proposed pages to the Profile page

---
+++ app/[hotelID]/[roomID]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomPage = () => {
+  return <div>RoomPage</div>;
+};
+
+export default RoomPage;
---
+++ app/[hotelID]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HotelPage = () => {
+  return <div>HotelPage</div>;
+};
+
+export default HotelPage;
