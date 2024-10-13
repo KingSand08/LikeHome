fcd6e87f02a9384d4b179958a6b396568c4f2038
Mau <79354255+Luceium@users.noreply.github.com>
2024-10-13 02:43:36 -0700

    Refactors page structure (#60)
    * Refactors page structure
    Adds new page for hotel room
    Reorganizes a few pages
    Adds new components that were proposed pages to the Profile page
    
    * adds slugs and error handling to hotel pages and room pages
    
    * adds slug and error handling to bookings
    
    * creates strucure for suspense and fallbacks on main page
    
    * add link to improvement
    
    * adds bookings/[id] error.tsx file
    
    * fix buld errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Link from "next/link";
+import { Suspense } from "react";
 
 export default function HomePage() {
+  const hotels = ["a", "b", "c"];
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-    </div>
+    <main className="">
+      Main Page
+      {hotels.map((hotel) => (
+        <Suspense key={hotel} fallback={<h1>loading...</h1>}>
+          {/* Allows hotels to be loaded individually and not impede the first contentful paint*/}
+          {/* This is a perfect oppportunity to use https://react.dev/reference/react/Suspense#showing-stale-content-while-fresh-content-is-loading */}
+          <div key={hotel}>{hotel}</div>
+        </Suspense>
+      ))}
+    </main>
   );
 }
