2f3ebe894f89284a7693af77acd070ac6b9ab13d
Mau <79354255+Luceium@users.noreply.github.com>
2024-10-13 02:10:31 -0700

    creates strucure for suspense and fallbacks on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-
+import { Suspense } from "react";
 
 export default function HomePage() {
-  return <main className="">Main Page</main>;
+  const hotels = ["a", "b", "c"];
+  return (
+    <main className="">
+      Main Page
+      {hotels.map((hotel) => (
+        <Suspense fallback={<h1>loading...</h1>}>
+          {/* Allows hotels to be loaded individually and not impede the first contentful paint*/}
+          <div key={hotel}>{hotel}</div>
+        </Suspense>
+      ))}
+    </main>
+  );
 }
