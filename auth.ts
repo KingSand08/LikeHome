d23489338d2c601efd25709e8224fafa0763c586
Connor L <72523745+KingSand08@users.noreply.github.com>
2024-10-16 10:27:55 -0700

    New OAuth Authentication, Profile Acknowledgement, and  Dynamic Sign In/Out Button (#63)
    * Redid authentication. Everything works now. Now has signout and profile button in nav and has way less lines.
    
    * Fixed deployment bug with image

--- auth.ts
+++ auth.ts
@@ -1,46 +1,6 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-import Credentials from "next-auth/providers/credentials"
-import type { Provider } from "next-auth/providers"
 
- 
-const providers: Provider[] = [
-  Credentials({
-    credentials: { password: { label: "Password", type: "password" } },
-    authorize(c) {
-      if (c.password !== "password") return null
-      return {
-        id: "test",
-        name: "Test User",
-        email: "test@example.com",
-      }
-    },
-  }),
-  Google
-]
- 
-export const providerMap = providers
-  .map((provider) => {
-    if (typeof provider === "function") {
-      const providerData = provider()
-      return { id: providerData.id, name: providerData.name }
-    } else {
-      return { id: provider.id, name: provider.name }
-    }
-  })
-  .filter((provider) => provider.id !== "credentials")
- 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers,
-  pages: {
-    signIn: "/signin",
-  },
-  callbacks: {
-        async redirect({ url, baseUrl }) {
-            if (url.includes('errror=access_denied')) {
-                return '/signin'
-            }
-            return url
-        },
-    },
-});
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google],
+})
