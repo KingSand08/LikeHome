4922123891dcf22ed7cb0e21b4d98038a23e7908
kingsand <clinvil2@gmail.com>
2024-10-30 13:22:41 -0700

    Removed the auth.config.ts dependency on emailer (moved to adapter-auth-bugs) branch.

--- auth.config.ts
+++ auth.config.ts
@@ -5,9 +5,6 @@ import type { NextAuthConfig } from "next-auth"
 export default {
     providers: [
         Google,
-        // Resend({
-        //     from: 'LikeHome <onboarding@resend.dev>',
-        // }),
     ], pages: {
         signIn: "/signin",
         error: "/signin",
