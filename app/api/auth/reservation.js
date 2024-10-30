2503842cb24f05035b4484924f5ce0d2cf8e5e64
Ahsan <ahsanali2001@gmail.com>
2024-10-30 11:50:13 -0700

    Reservation System Implementation (For Testing)

---
+++ app/api/auth/reservation.js
@@ -0,0 +1,11 @@
+//File: pages/reservation.js
+import ReservationForm from '../components/ReservationForm';
+
+export default function ReservationPage() {
+  return (
+    <div>
+      <h1>Make a Reservation</h1>
+      <ReservationForm />
+    </div>
+  );
+}
