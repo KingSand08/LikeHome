659eea814c4b439516f94bc2a657612ff2bcc685
ray-sjsu <raymund.mercader@sjsu.edu>
2024-11-14 10:07:48 -0800

    Revise page structure to include API changes

--- __tests__/ExampleUnit.test.tsx
+++ __tests__/ExampleUnit.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Page from "../app/page";
+import Page from "../app/(old)/home/page";
 import exp from "constants";
 
 describe("h1", () => {
