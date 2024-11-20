9c4759dd216c7df69d8a386bb9b1f0ea5e851537
Hoa Nguyen <hoatuongminh@gmail.com>
2024-11-20 15:38:10 -0800

    changed some code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,55 +9,55 @@ const team = [
     image: "/images/team/mau.png",
     description:
       "Project Manager overseeing development and project milestones",
-    email: "",
+    linkedin: "",
   },
   {
     name: "Ahsan Ali",
     image: "/images/team/Ahsan.png",
     description:
       "Backend Developer specializing in server-side development and testing.",
-    email: "",
+    linkedin: "",
   },
   {
     name: "Ali Altimimi",
     image: "/images/team/Ali.png",
     description:
       "Backend Developer focused on database architecture and optimization.",
-    email: "",
+    linkedin: "",
   },
   {
     name: "Connor Linville",
     image: "/images/team/Connor.png",
     description:
       "Backend Developer with expertise in deployment strategies and user authentication systems.",
-    email: "",
+    linkedin: "",
   },
   {
     name: "Steven Lu",
     image: "/images/team/Steven.png",
     description:
       "Frontend Developer responsible for implementing designs from Figma into a responsive UI.",
-    email: "",
+    linkedin: "",
   },
   {
     name: "Raymund Mercader",
     image: "/images/team/Raymund.png",
     description:
       "Full Stack Developer experienced in both backend and frontend development.",
-    email: "",
+    linkedin: "",
   },
   {
     name: "Hoa Tuong Minh Nguyen",
     image: "/images/team/Minh.png",
     description:
       "UI/UX Designer on Figma and Frontend Developer.",
-    email: "hoatuongminh@gmail.com",
+    linkedin: "https://www.linkedin.com/in/hoa-nguyen-m1000/",
   },
   {
     name: "Ryan Tang",
     image: "/images/team/Ryan.png",
     description: "Backend Developer focused on stripe and prisma integration.",
-    email: "",
+    linkedin: "",
   },
 ];
 
@@ -68,19 +68,31 @@ const AboutPage = () => {
       <div className="flex flex-col lg:flex-row gap-8">
         {/* Contact Us Form */}
         <div className="bg-white p-6 rounded-lg shadow-lg flex-[1] lg:flex-[0.4]">
-          <h2 className="text-3xl font-bold mb-4 text-center">Contact Us!</h2>
+          <h2 className="text-3xl font-bold mb-20 text-center">Contact Us!</h2>
           <form>
-            <div className="mb-4 ">
+            <div className="mb-8 ">
               <label htmlFor="name" className="block text-sm font-medium text-gray-700 mb-1">
                 Name
               </label>
               <input
                 type="text"
                 id="name"
-                className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+                className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"            
               />
+              
             </div>
-            <div className="mb-4 ">
+            <div className="mb-8 ">
+              <label htmlFor="Surname" className="block text-sm font-medium text-gray-700 mb-1">
+                Surname
+              </label>
+              <input
+                type="text"
+                id="Surname"
+                className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"            
+              />
+              
+            </div>
+            <div className="mb-8 ">
               <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
                 Email
               </label>
@@ -90,17 +102,17 @@ const AboutPage = () => {
                 className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            <div className="mb-4 ">
+            <div className="mb-8 ">
               <label htmlFor="message" className="block text-sm font-medium text-gray-700 mb-1">
                 Message
               </label>
               <textarea
                 id="message"
-                rows={4}
+                rows={8}
                 className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
               ></textarea>
             </div >
-            <div className="flex justify-center">
+            <div className="flex justify-center mb-8">
               <button
                 type="submit"
                 className=" bg-black text-white py-2 px-20 rounded-md hover:bg-blue-600 w-auto"
@@ -137,8 +149,13 @@ const AboutPage = () => {
                 <h3 className="text-xl font-semibold mb-2">{member.name}</h3>
                 <p>{member.description}</p>
                 <p className="text-sm text-gray-600">
-                  <a href={`mailto:${member.email}`} className="hover:text-blue-500">
-                    {member.email}
+                  <a
+                    href={member.linkedin}
+                    className="hover:text-blue-500"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    My LinkedIn Profile
                   </a>
                 </p>
               </div>
