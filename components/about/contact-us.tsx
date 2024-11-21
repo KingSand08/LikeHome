fdf1dccb9eb402d14675e4ebe561e50fb61e3e65
luSteven01 <steven.lu01@sjsu.edu>
2024-11-20 17:32:45 -0800

    implemented daisyui theme to contact us page

--- components/about/contact-us.tsx
+++ components/about/contact-us.tsx
@@ -4,8 +4,10 @@ import React from "react";
 
 const ContactUs = () => {
   return (
-    <div className="bg-white p-6 mt-10 rounded-lg shadow-lg flex-[1] lg:flex-[0.4] max-w-screen-md ml-auto mr-auto">
-      <h2 className="text-3xl font-bold mb-4 text-center">Contact Us!</h2>
+    <div className="bg-base-100 p-6 mt-10 rounded-lg shadow-lg flex-[1] lg:flex-[0.4] max-w-screen-md mx-auto">
+      <h2 className="text-3xl font-bold mb-4 text-center text-primary">
+        Contact Us!
+      </h2>
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -16,49 +18,49 @@ const ContactUs = () => {
           });
         }}
       >
-        <div className="mb-4 ">
+        <div className="mb-4">
           <label
             htmlFor="name"
-            className="block text-sm font-medium text-gray-700 mb-1"
+            className="block text-sm font-medium text-base-content mb-1"
           >
             Name
           </label>
           <input
             type="text"
             name="name"
-            className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+            className="bg-base-200 text-base-content w-full border border-base-300 rounded-md p-2 focus:ring focus:ring-primary focus:outline-none"
           />
         </div>
-        <div className="mb-4 ">
+        <div className="mb-4">
           <label
             htmlFor="email"
-            className="block text-sm font-medium text-gray-700 mb-1"
+            className="block text-sm font-medium text-base-content mb-1"
           >
             Email
           </label>
           <input
             type="email"
             name="email"
-            className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+            className="bg-base-200 text-base-content w-full border border-base-300 rounded-md p-2 focus:ring focus:ring-primary focus:outline-none"
           />
         </div>
-        <div className="mb-4 ">
+        <div className="mb-4">
           <label
             htmlFor="message"
-            className="block text-sm font-medium text-gray-700 mb-1"
+            className="block text-sm font-medium text-base-content mb-1"
           >
             Message
           </label>
           <textarea
             name="message"
             rows={4}
-            className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+            className="bg-base-200 text-base-content w-full border border-base-300 rounded-md p-2 focus:ring focus:ring-primary focus:outline-none"
           ></textarea>
         </div>
         <div className="flex justify-center">
           <button
             type="submit"
-            className=" bg-black text-white py-2 px-20 rounded-md hover:bg-blue-600 w-auto"
+            className="btn btn-primary px-10"
           >
             Submit
           </button>
