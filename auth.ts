b6c4ee9c1c658eec9bbb44ab8aae9cf73d2e5a6c
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-11-24 14:24:34 -0800

    adds event and creates user

--- auth.ts
+++ auth.ts
@@ -1,27 +1,32 @@
-import NextAuth from "next-auth"
-import type { Provider } from "next-auth/providers"
-import Google from "next-auth/providers/google"
-import Discord from "next-auth/providers/discord"
+import NextAuth, { Account, User } from "next-auth";
+import type { Provider } from "next-auth/providers";
+import Google from "next-auth/providers/google";
+import Discord from "next-auth/providers/discord";
+import { createUser } from "./server-actions/user-actions";
 
-const providers: Provider[] = [
-  Google,
-  Discord,
-]
+const providers: Provider[] = [Google, Discord];
 
 export const providerMap = providers
   .map((provider) => {
     if (typeof provider === "function") {
-      const providerData = provider()
-      return { id: providerData.id, name: providerData.name }
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
     } else {
-      return { id: provider.id, name: provider.name }
+      return { id: provider.id, name: provider.name };
     }
   })
-  .filter((provider) => provider.id !== "credentials")
+  .filter((provider) => provider.id !== "credentials");
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers,
   pages: {
     signIn: "/signin",
   },
-})
+  events: {
+    signIn: async (m: { user: User }) => {
+      console.log(m.user);
+      createUser(m.user.email!, m.user.name!);
+      console.log();
+    },
+  },
+});
--- server-actions/user-actions.ts
+++ server-actions/user-actions.ts
@@ -1,33 +1,36 @@
 "use server";
-import { auth } from "@/auth";
 
-export async function createUser(id: string, email: string, name: string) {
-    // check if user already exists
-    const user = await prisma.user.findUnique({
-        where: { email },
-    });
-    if (user) {
-        return user;
-    }
-    
-    return prisma.user.create({
-        data: {
-            name,
-            email,
-            rewardPoints: 0
-        },
-    })
+import prisma from "../lib/prisma";
+
+export async function createUser(email: string, name: string) {
+  // check if user already exists
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (user) {
+    return user;
+  }
+
+  return prisma.user.create({
+    data: {
+      name,
+      email,
+      rewardPoints: 0,
+    },
+  });
 }
 
 export async function updatePoints(userId: string, pointChange: number) {
-    const oldPoints = await prisma.user.findUnique({
-        where: { id: userId },
-        select: { points: true },
-        });
-    
-    const points = oldPoints + pointChange;
+  const oldPoints = (
+    await prisma.user.findUnique({
+      where: { id: userId },
+    })
+  )?.rewardPoints;
+
+  const rewardPoints = oldPoints ?? 0 + pointChange;
 
   return prisma.user.update({
     where: { id: userId },
-    data: { points },
-});}
+    data: { rewardPoints },
+  });
+}
