de6cce85c1f51937e6a6d3d31c715e6c4e87ae9c
Mau <mauriciom.curiel@gmail.com>
2024-11-25 08:31:40 -0800

    Prisma (#97)
    * sets up prisma
    
    * adds sign in event and creates users
    
    * Setup for future reservation implementation (will be in a separate pr)
    
    ---------
    
    Co-authored-by: ryanhtang <ryanhitang@gmail.com>
    Co-authored-by: ray-sjsu <raymund.mercader@sjsu.edu>

--- auth.ts
+++ auth.ts
@@ -1,27 +1,30 @@
-import NextAuth from "next-auth"
-import type { Provider } from "next-auth/providers"
-import Google from "next-auth/providers/google"
-import Discord from "next-auth/providers/discord"
+import NextAuth from "next-auth";
+import type { Provider } from "next-auth/providers";
+import Google from "next-auth/providers/google";
+import Discord from "next-auth/providers/discord";
+import { createUser } from "./server-actions/user-actions";
 
-const providers: Provider[] = [
-  Google,
-  Discord,
-]
+const providers: Provider[] = [Google, Discord];
 
 export const providerMap = providers
   .map((provider) => {
     if (typeof provider === "function") {
-      const providerData = provider()
-      return { id: providerData.id, name: providerData.name }
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
     } else {
-      return { id: provider.id, name: provider.name }
+      return { id: provider.id, name: provider.name };
     }
   })
-  .filter((provider) => provider.id !== "credentials")
+  .filter((provider) => provider.id !== "credentials");
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers,
   pages: {
     signIn: "/signin",
   },
-})
+  events: {
+    async signIn(user) {
+      await createUser(user.user.email!);
+    },
+  },
+});
