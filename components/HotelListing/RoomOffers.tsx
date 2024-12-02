1353fc9b1434af929f7e37b581f237d968ec7519
kingsand <clinvil2@gmail.com>
2024-12-02 02:29:32 -0800

    All room images now are mobile friendly.

--- components/HotelListing/RoomOffers.tsx
+++ components/HotelListing/RoomOffers.tsx
@@ -3,46 +3,20 @@ import HotelRoomList from "../booking/HotelRooms/HotelRoomList";
 import { APIHotelRoomOffersJSONFormatted } from "@/app/api/hotels/search/rooms/route";
 
 
-const RoomOffers: React.FC<{ hotelRooms: APIHotelRoomOffersJSONFormatted }> = ({ hotelRooms }) => {
+const RoomOffers: React.FC<{ hotelRooms: APIHotelRoomOffersJSONFormatted }> = ({
+  hotelRooms,
+}) => {
   return (
     <>
-      <div className="mb-8 bg-slate-300 dark:bg-slate-700 shadow-lg rounded-xl p-6">
-        <h2 className="text-2xl font-semibold mb-4 text-base-content">Room Offers</h2>
+      <div className="mb-8 bg-gradient-to-r from-slate-700 to-slate-600 shadow-lg rounded-xl p-6">
         {hotelRooms ? (
-          <div>
-            <p className="text-base-content mb-6 font-bold">
-              Base Price Per Night:{" "}
-              <span className="font-semibold">
-                {hotelRooms.basePricePerNight}
-              </span>
-            </p>
-            <div>
-              <h3 className="text-xl text-base-content font-semibold mb-4">Room List</h3>
-              <HotelRoomList rooms={hotelRooms.hotelRoomOffers} />
-            </div>
-          </div>
+          <HotelRoomList rooms={hotelRooms.hotelRoomOffers} />
         ) : (
-          <p className="text-base-content">No rooms available</p>
+          <p className="text-gray-100">No rooms available</p>
         )}
       </div>
     </>
-  )
-}
-
-// const RoomOffers: React.FC<{ hotelRooms: APIHotelRoomOffersJSONFormatted }> = ({
-//   hotelRooms,
-// }) => {
-//   return (
-//     <>
-//       <div className="mb-8 bg-gradient-to-r from-slate-700 to-slate-600 shadow-lg rounded-xl p-6">
-//         {hotelRooms ? (
-//           <HotelRoomList rooms={hotelRooms.hotelRoomOffers} />
-//         ) : (
-//           <p className="text-gray-100">No rooms available</p>
-//         )}
-//       </div>
-//     </>
-//   );
-// };
+  );
+};
 
 export default RoomOffers;
